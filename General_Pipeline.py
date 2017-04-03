00ef94a31fb5647684591dcc697cd0bdcfe5e2a2
RoelBouman <roelbouman@gmail.com>
2017-04-03 18:26:34 +0200

    Updated general pipeline to incorporate new feature extraction methods using pickle

--- General_Pipeline.py
+++ General_Pipeline.py
@@ -8,40 +8,46 @@ Created on Thu Feb 23 11:24:00 2017
 #Load libraries
 import cPickle as pickle
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier as RF
+import pandas as pd
+import os
+from sklearn.linear_model import LogisticRegression
 #from sklearn.ensemble import DecisionTreeClassifier as RF
 #from sklearn.neighbors import KNeighborsClassifier as RF
-from scipy.sparse import hstack
 from sklearn.metrics import accuracy_score as ACC
 from sklearn.metrics import log_loss
 from sklearn.metrics import confusion_matrix as  CM
-from sklearn import preprocessing 
+#from sklearn import preprocessing 
 from sklearn.model_selection import StratifiedKFold as SKF
-from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import TfidfVectorizer
 import scipy as sp
 
-#make vectorizer for tfidf processing
-vectorizer = TfidfVectorizer(analyzer = "word")
-#%% logloss function
-def logloss(act, pred):
-    epsilon = 1e-15
-    pred = sp.maximum(epsilon, pred)
-    pred = sp.minimum(1-epsilon, pred)
-    ll = sum(act*sp.log(pred) + sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
-    ll = ll * -1.0/len(act)
-    return ll
-#%%
-#Load Features, Data and labels.
 
-apps_clean = pickle.load(open("stringdumpfinal.p","rb"))
-#tfidf_features = pickle.load(open('termmat.p', 'rb'))
-labels = pickle.load(open('labels.p', 'rb'))
+#%% Load general information and data
+gendir = './general/'
 
-#transform to numerical, one-hot, labels
-lb = preprocessing.LabelBinarizer()
-templabels=lb.fit_transform(labels) #also use to transform back
-templabels = templabels*np.arange(templabels.shape[1])
-numlabels = np.sum(templabels,axis=1)
+train_labels = pickle.load(open(gendir + 'labels.p',"rb"))
+nclasses = pickle.load(open(gendir + 'nclasses.p',"rb"))
+label_encoding = pickle.load(open(gendir + 'label_encoding.p',"rb"))
+device_names = pickle.load(open(gendir + 'device_names.p',"rb"))
+
+#%%Load Features 
+
+train_features = []
+feature_names = []
+
+for filename in os.listdir('./Train_features/'):
+        train_features.append(pickle.load(open('./Train_features/' + filename,"rb")))
+        feature_names.append(filename)
+    
+test_features = []
+
+for filename in os.listdir('./Test_features/'):
+        test_features.append(pickle.load(open('./Test_features/' + filename,"rb")))
+    
+#%% Combine Features
+
+X_train_full = sp.sparse.hstack(train_features, format = 'csr')
+X_test_full = sp.sparse.hstack(test_features, format = 'csr')
 
 #%%
 #Create cross-validation splits (10-fold)
@@ -53,55 +59,46 @@ skf1 = SKF(n_splits = n)
 test_acc = np.zeros(n)
 loss = np.zeros(n)
 
-confmat = np.zeros((templabels.shape[1],templabels.shape[1]))
+confmat = np.zeros((len(np.unique(train_labels)),len(np.unique(train_labels))))
 i=0
 
 #Single loop: testing is done by submitting near-optimal results to Kaggle.
-for train_index, test_index in skf1.split(np.zeros(len(numlabels)),numlabels):
-     print 'start new it'
-     #-----------------------------------------------------------
-     #Construct final feature vectors 
-     #This is done in the loop because of tf-idf features. If features are only device
-     #dependent, can be moved outside of the loop.
-     #-----------------------------------------------------------
-     #concatenate and constructfeatures (more can be added by using hstack())
-     docs_train = [apps_clean[idx] for idx in train_index]
-     docs_test = [apps_clean[idx] for idx in test_index]
-          
-     #X_train = hstack((vectorizer.fit_transform(apps_clean[train_index]),~~~))
-     #X_test = hstack((vectorizer.transform(apps_clean[test_index]), ~~~))
+for train_index, test_index in skf1.split(np.zeros(len(train_labels)),train_labels):
+     print 'start iteration ' + str(i)
      
-     X_train = vectorizer.fit_transform(docs_train)
-     X_test = vectorizer.transform(docs_test)
+     X_train = X_train_full[train_index]
+     X_test = X_train_full[test_index]
      
-     Y_train = numlabels[train_index]
-     Y_test = numlabels[test_index]
+     Y_train = train_labels[train_index]
+     Y_test = train_labels[test_index]
      
      #-----------------------------------------------------------
      #Train classifier
      #-----------------------------------------------------------     
      
-     weights = {}
-     for c in np.arange(len(np.unique(numlabels))):
-         weights[c] = len(Y_train)/np.double(len(np.unique(numlabels))*np.bincount(Y_train))[c]
-    
-     rf = RF(n_estimators=1000, class_weight=weights)
-     rf.fit(X_train,Y_train)
+     #clf = RF(n_estimators=100, class_weight='balanced')
+     clf = LogisticRegression(C=0.02, multi_class='multinomial',solver='lbfgs', class_weight='balanced')
+     clf.fit(X_train,Y_train)
      
      #-----------------------------------------------------------
      #Evaluate classifier
      #-----------------------------------------------------------
-     test_pred = rf.predict(X_test)
-     test_proba = rf.predict_proba(X_test)
+     test_pred = clf.predict(X_test)
+     test_proba = clf.predict_proba(X_test)
      test_acc[i] = ACC(Y_test,test_pred)
-     loss[i] = log_loss(numlabels[test_index],test_proba)
+     loss[i] = log_loss(train_labels[test_index],test_proba)
      confmat = confmat + CM(Y_test,test_pred)
      i=i+1
-     print 'end it'
+     print 'end iteration ' + str(i)
      
      
 avg_error = np.mean(test_acc)
 avg_loss = np.mean(loss)
-#%%
-#Upload promising results
-#save settings.
+#%% Final prediction and csv saving
+print 'starting final prediction'
+#clf = RF(n_estimators=100, class_weight='balanced')
+LogisticRegression(C=0.02, multi_class='multinomial',solver='lbfgs', class_weight='balanced')
+clf.fit(X_train_full,train_labels)
+pred = pd.DataFrame(clf.predict_proba(X_test_full), index = device_names, columns=label_encoding)
+
+pred.to_csv('testsub.csv',index=True)
