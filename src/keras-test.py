aa4ec2f6cab2c62ca1b1b59ba77588d7c3ad7e62
Thijs Werrij <thijswerrij@planet.nl>
2017-06-12 17:39:27 +0200

    keras-test now has functioning cross-validation, also uses the RSMLE to score

--- src/keras-test.py
+++ src/keras-test.py
@@ -13,6 +13,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers.advanced_activations import PReLU
 from sklearn.model_selection import StratifiedKFold as SKF
+from evaluate_validation_predictions import eval_rmsle
 
 #%% Code for fiting from generator by Chenglong Chen
 # (https://www.kaggle.com/c/talkingdata-mobile-user-demographics/forums/t/22567/neural-network-for-sparse-matrices)
@@ -69,31 +70,31 @@ indexes         = testset.iloc[:,0]
 def base_model():
     input_shape = X_train_full.shape[1:]
     model = Sequential()
-    model.add(Dense(100, kernel_initializer='normal', input_shape=input_shape))
+    model.add(Dense(473, kernel_initializer='normal', input_shape=input_shape))
+    model.add(PReLU())
     model.add(Dropout(0.2))
-    model.add(Dense(75, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(200, kernel_initializer='normal', activation='relu'))
+    model.add(PReLU())
+    model.add(Dropout(0.2))
+    model.add(Dense(100, kernel_initializer='normal', activation='relu'))
+    model.add(PReLU())
     model.add(Dropout(0.2))
     model.add(Dense(1, kernel_initializer='normal'))
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
-#%%
+#%% Cross-validation
 #Create cross-validation splits (n-fold)
-n = 5
+
+n = 3
 skf1 = SKF(n_splits = n)
 
-#%%
-#Cross validating loop
-test_acc = np.zeros(n)
-loss = np.zeros(n)
-
-#%% Training
-
-model = base_model()
+score = np.zeros(n)
 i = 0
 
 for train, test in skf1.split(X_train_full, labels[:,0]):
-    print ('start iteration ' + str(i+1))
+    model = base_model()
+    print ('start iteration ' + str(i+1) + ' of ' + str(n))
     
     X_train = X_train_full[train]
     X_test  = X_train_full[test]
@@ -102,19 +103,27 @@ for train, test in skf1.split(X_train_full, labels[:,0]):
     Y_test  = labels[test]
     
     model.fit_generator(generator=batch_generator(X_train, Y_train, 100, True),
-                        epochs=5,
+                        epochs=3,
                         validation_data=(X_test, Y_test),
-                        steps_per_epoch=2000)
-    #model.fit(X_train[train], labels[train],
-    #          validation_data=(X_train[test],labels[test]),
-    #          epochs=5, batch_size=10)
+                        steps_per_epoch=2000)#X_train.shape[0]/n)
+    subset_pred = model.predict(X_test)
+    score[i] = eval_rmsle(subset_pred[i],Y_test)
     i+=1
 
-pred = model.predict_generator(generator=batch_generatorp(X_test_full, 200, False),
+avg_score = np.mean(score)
+
+#%% Final prediction & save submission
+
+model = base_model()
+model.fit_generator(generator=batch_generator(X_train_full, labels, 100, True),
+                        epochs=10,
+                        validation_data=(X_test, Y_test),
+                        steps_per_epoch=X_train_full.shape[0]/10)
+
+final_pred = model.predict_generator(generator=batch_generatorp(X_test_full, 200, False),
                                steps=X_test_full.shape[0])
 
-#%% Save file
-sub = pd.DataFrame(pred, index=indexes, columns=['price_doc'])
+sub = pd.DataFrame(final_pred, index=indexes, columns=['price_doc'])
 sub.index.names = ['id']
 
 sub_file = 'DL_sub.csv'
