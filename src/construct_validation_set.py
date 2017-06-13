3487c045912c3d427d3631f27992636d010540be
RoelBouman <roelbouman@gmail.com>
2017-06-13 14:44:08 +0200

    training data now based on 2014 and test set on 2015 only.

--- src/construct_validation_set.py
+++ src/construct_validation_set.py
@@ -10,20 +10,21 @@ import pandas as pd
 # Declare paths
 data_folder_loc = "../data/"
 train_loc = data_folder_loc + "train.csv"
-train_subset_loc = data_folder_loc + "train_subset.csv"
-validation_loc = data_folder_loc + "validation.csv"
+train_subset_loc = data_folder_loc + "train_2014.csv"
+validation_loc = data_folder_loc + "test_2015.csv"
 val_prices_loc = data_folder_loc + "val_prices.csv"
 
 train = pd.read_csv(train_loc)
 
 #Split on instances before(<) and in(>=) 2015
 train_subset = train[train['id'] < 27235]
+train_2014 = train_subset[train_subset['id'] >13572]
 validation = train[train['id'] >= 27235]
 
 val_prices = validation['price_doc']
 
 validation = validation.drop('price_doc', 1)
 
-train_subset.to_csv(train_subset_loc)
+train_2014.to_csv(train_subset_loc)
 validation.to_csv(validation_loc)
 val_prices.to_csv(val_prices_loc)
