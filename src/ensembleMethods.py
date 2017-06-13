2cf267e553db0d3ae3c046e3599c272e2a5ad8ea
Jeff Luppes <jeffluppes@gmail.com>
2017-06-13 14:54:27 +0200

    ensembleMethods.py: added RF with csv being redirected to /output/

---
+++ src/ensembleMethods.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+import datetime
+
+print('\nWelcome to "duurt laang" ETL services. \n\n..Reading in training data as X_train..')
+X_train = pd.read_csv('../data/one-hot_median_filled_train.csv')
+
+print('..Loading test set as X_test')
+tX_test = pd.read_csv('../data/one-hot_median_filled_test.csv')
+ids = tX_test['id']
+X_test = tX_test
+
+print('..Fetching true class labels')
+ty_train = pd.read_csv('../data/train_prices.csv')
+y_train = ty_train['price_doc'].values
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(ids.shape)
+
+# Random Forest 
+print('..RandomForestRegressor with n = 10 ..')
+clf = RandomForestRegressor(n_estimators=10)
+clf.fit(X_train, y_train)
+y_rf = clf.predict(X_test)
+print y_rf.shape
+
+#construct a sample submission for ensembling, per xgb-baseline2
+time = datetime.datetime.now().strftime("%A, %d %B %Y %I%M%p")
+temp = pd.DataFrame({'id': ids, 'price_doc': y_rf})
+temp.head()
+temp.to_csv('../output/RF_' + time + '.csv', index=False)
